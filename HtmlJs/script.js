4b86e93189a1dacc395ccbf90df898c4a283b5c5
Garima <garimakharbanda02@gmail.com>
2024-07-11 21:47:47 +0530

    htmlQues1

---
+++ HtmlJs/script.js
@@ -0,0 +1,6 @@
+let firstNumber = prompt("Enter the first integer:");
+firstNumber = parseInt(firstNumber)
+let secondNumber = prompt("Enter the second integer:");
+secondNumber = parseInt(secondNumber);
+let sum = firstNumber + secondNumber;
+alert(`The sum of ${firstNumber} and ${secondNumber} is ${sum}`);
